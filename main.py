c931a312dc308a2581dc95836dafb0859c85fd82
Aylton Almeida <ayltonalmeida2@gmail.com>
2021-02-11 21:33:17 -0300

    progressBar

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 import os
+import progressbar
 
 from pprint import pprint
 from dotenv import load_dotenv
@@ -36,16 +37,20 @@ def getQuery(cursor: str = None):
 
 repo_list: list = []
 
-print('Fetching repos...')
+# extremely necessary progress bar for better user experience
+widgets = [
+    '\x1b[33mFetching repos...\x1b[39m',
+    progressbar.Percentage(),
+    progressbar.Bar(marker='\x1b[32m#\x1b[39m'),
+]
+bar = progressbar.ProgressBar(
+    widgets=widgets, max_value=10, min_value=0).start()
 
-# Make 1000 requests
 for i in range(10):
+    # Make 1000 requests
 
     current_cursor = '"{}"'.format(repo_list[-1]['cursor']) if len(
         repo_list) > 0 else None
-
-    print('{} out of 1000. Current cursor: {}'.format(
-        (i + 1) * 100, current_cursor))
 
     query = getQuery(current_cursor)
 
@@ -62,8 +67,11 @@ for i in range(10):
     repo_data = json_data['data']['search']['edges']
 
     repo_list = [*repo_list, *repo_data]
+    bar.update(i + 1)
 
-print('All repos were fetch\n')
+bar.finish()
+
+print('All repos were fetch')
 print('Saving file...')
 
 data_frame = pd.DataFrame(repo_list)
